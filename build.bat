e5b10ea2c99e5b60ae047d97aed63d053ed38c23
BetterMap <88572536+BetterMapV2@users.noreply.github.com>
2023-07-11 17:30:57 +1200

    Add files via upload

---
+++ build.bat
@@ -0,0 +1,1 @@
+npx babel --delete-dir-on-start --retain-lines . -d "out/BetterMap" --copy-files --no-copy-ignored --ignore "**/babal-substitute-mappings/**","**/.git/**","**/build.bat","**/init.bat","**/node_modules/**"
